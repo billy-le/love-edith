a90a4de0fead0385dce2274f33b812019b4af149
Billy Le <lebilly87@gmail.com>
2021-02-27 09:09:51 -0800

    updated tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  theme: {},
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
