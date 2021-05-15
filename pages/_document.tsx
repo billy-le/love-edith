97f3468cbfcbb3842724b6afe5e49bfc33dea5b5
Billy Le <lebilly87@gmail.com>
2021-05-15 05:52:48 -0700

    placed google analytics in _document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script async src='https://www.googletagmanager.com/gtag/js?id=G-WCYJV54XJ4'></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WCYJV54XJ4', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          ></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
